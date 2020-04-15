88d8c7cb64a3eba0a75335c6148284dcb8e29ff5
LanskovNV <leins275@gmail.com>
2020-04-15 11:28:33 +0300

    Refactored Rectangle class  - change layers structure dependence of orientation

--- lab_3/python/main.py
+++ lab_3/python/main.py
@@ -6,7 +6,8 @@ from interpolation.interpolation import Interpolation
 
 if __name__ == "__main__":
     def function(x, y): return sin(x - y)
-    rect_shape = [[1, 1], [1, 3], [2, 3], [2, 1]]
+    # rect_shape = [[1, 1], [1, 3], [2, 3], [2, 1]]
+    rect_shape = [[1, 1], [1, 2], [3, 2], [3, 1]]
     num_of_nodes = 10
 
     inter = Interpolation(function, np.asarray(rect_shape))
@@ -14,7 +15,6 @@ if __name__ == "__main__":
     inter.omega.triangulate(num_of_nodes)
     inter.omega.draw_net()
 
-# Задать исследуемую функцию
 # Построить линейный интерполянт
 # Численно посчитать градиент
 # Численно посчитать интеграл
--- lab_3/python/shapes/rectangle.py
+++ lab_3/python/shapes/rectangle.py
@@ -15,18 +15,31 @@ class Rectangle(Region):
         def half_step(interval):
             return (interval[1] - interval[0]) / (n - 1) / 2
 
-        first_layer = np.linspace(self.x_int[0], self.x_int[1], n)
-        second_layer = np.linspace(self.x_int[0] + half_step(self.x_int), self.x_int[1] + half_step(self.x_int), n - 1,
-                                   endpoint=False)
+        def get_x(interval):
+            first_layer = np.linspace(interval[0], interval[1], n)
+            hs = half_step(interval)
+            second_layer = np.linspace(interval[0] + hs, interval[1] + hs, n - 1, endpoint=False)
 
-        second_layer = np.append(second_layer, first_layer[0])
-        second_layer = np.append(second_layer, first_layer[-1])
+            second_layer = np.append(second_layer, first_layer[0])
+            second_layer = np.append(second_layer, first_layer[-1])
 
-        segment = np.append(first_layer, second_layer)
-        net_x = np.tile(segment, n - 1)
-        self.x_coords = np.append(net_x, first_layer)
+            segment = np.append(first_layer, second_layer)
+            net_x = np.tile(segment, n - 1)
+            return np.append(net_x, first_layer)
 
-        net_y = np.linspace(self.y_int[0], self.y_int[1], 2*n - 1)
-        first = np.repeat(net_y[::2], n)
-        second = np.repeat(net_y[1::2], n + 1)
-        self.y_coords = np.sort(np.append(first, second))
+        def get_y(interval):
+            net_y = np.linspace(interval[0], interval[1], 2*n - 1)
+            first = np.repeat(net_y[::2], n)
+            second = np.repeat(net_y[1::2], n + 1)
+            return np.sort(np.append(first, second))
+
+        def get_coords(x_int, y_int):
+            xc = get_x(x_int)
+            yc = get_y(y_int)
+            return xc, yc
+
+        # longest edge should have more points
+        if (self.x_int[1] - self.x_int[0]) > (self.y_int[1] - self.y_int[0]):
+            self.y_coords, self.x_coords = get_coords(self.y_int, self.x_int)
+        else:
+            self.x_coords, self.y_coords = get_coords(self.x_int, self.y_int)
